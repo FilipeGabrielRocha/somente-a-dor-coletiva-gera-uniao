86aa1205e212791d824188956ce84a1a5ffd2f50
FilipeGabrielRocha <filiperochaprogramador@gmail.com>
2023-11-28 10:09:10 -0300

    Exercícios de Java

---
+++ ListaDeExercicio/src/somenteADorColetivaGeraUniao/Ex07.java
@@ -0,0 +1,31 @@
+package somenteADorColetivaGeraUniao;
+
+public class Ex07 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            if (i == 3) {
+                break;
+            }
+
+            switch (i) {
+                case 1:
+                    System.out.println("Número 1 - É o número 1");
+                    break;
+                case 2:
+                    System.out.println("Número 2 - É o número 2");
+                    break;
+                case 3:
+                    System.out.println("Número 3 - É o número 3");
+                    break;
+                case 4:
+                    System.out.println("Número 4 - É o número 4");
+                    break;
+                case 5:
+                    System.out.println("Número 5 - É o número 5");
+                    break;
+                default:
+                    System.out.println("Número desconhecido");
+            }
+        }
+    }
+}
